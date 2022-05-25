3087e2f817fae507c9455b6afd57450cbbc48446
rishabh-sharma4 <rishabh1010sharma@gmail.com>
2022-05-25 16:16:21 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={id:'',users:null}  
+  }
+  
+  submitClick=(e)=>{
+    e.preventDefault();
+   /*fetch(`https://jsonplaceholder.typicode.com/todos/${this.state.id}`)
+    .then(
+      (res)=>{res.json().then((res)=>{
+        this.setState({users:res})
+      })}
+    )*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    axios.get(`https://jsonplaceholder.typicode.com/todos/${this.state.id}`)
+    .then(res=>{
+      console.log(res);
+      this.setState({users:res.data})
+    })
+  }
+
+  handleId =(e)=>{
+    this.setState({id:e.target.value})
+    try{
+      if(e.target.value==='') throw "is empty";
+      else if(e.target.value>200 || e.target.value<1) throw "should be between 1 to 200";
+      else document.getElementById("msg").innerHTML="";
+    }
+    catch(err){
+      document.getElementById("msg").innerHTML="Id"+err;
+    }
+    /*if(e.target.value===""){
+      document.getElementById("msg").innerHTML="Id is empty";
+      }
+    else if(e.target.value>200 || e.target.value<1){
+      document.getElementById("msg").innerHTML="Id should be between 1 to 200";
+    }
+    else{
+    document.getElementById("msg").innerHTML="";
+    }*/
+
+    
+  }
+ componentDidUpdate(){
+    if(this.state.users!=null){
+    document.getElementById("mydiv").innerHTML=
+    "userId:"+this.state.users.userId+"<br/>"+
+    "id:"+this.state.users.id+"<br/>"+
+    "title:"+this.state.users.title+"<br/>"+
+    "completed:"+this.state.users.completed;
+  }}
+
+  render() {
+    return( 
+      <div className="App">
+        <h1>
+          API data
+        </h1>
+        <form onSubmit={this.submitClick}>
+          <input type="number" onChange={this.handleId}/>
+          <input type="submit"/>
+        </form> 
+        <div id="mydiv"></div>
+        <div id="msg">Id is empty</div>
+      </div>
+
+    )
+
+  }
+    
+
+  }
+
 
 export default App;
